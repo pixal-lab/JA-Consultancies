ec05faf72df2846dc20e49a378cfb9a2d810181e
Pablo Ahumada <pablo.ahumada1@mail.udp.cl>
2025-12-15 00:33:43 -0300

    Add scroll to top behavior

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './components/ScrollToTop'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <BrowserRouter basename={import.meta.env.BASE_URL}>
+            <ScrollToTop />
             <App />
         </BrowserRouter>
     </React.StrictMode>,
